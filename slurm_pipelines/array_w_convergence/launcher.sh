72ef28994274d0f76bdf392e282868f83eb20bfe
meekrob <davidcking.inbox@gmail.com>
2024-12-10 15:05:46 -0700

    try to detect alpine versus riviera

--- slurm_pipelines/array_w_convergence/launcher.sh
+++ slurm_pipelines/array_w_convergence/launcher.sh
@@ -1,12 +1,11 @@
 #!/usr/bin/env bash
 # launcher.sh - submit array job followed by converge script, dependent on array finishing afterok
-on_alpine=""
-if [ -n "$on_alpine" ]
+network_prefix=$(ip route show default | awk -F'[ .]' '{print $3 "." $4}')
+SERVER=$( ( [ "$network_prefix" = 129.82 ] && echo "CSU" ) || ( [ "$network_prefix" = 198.59 ] && echo "CU" ) || echo "unknown" )
+if [ "$SERVER" = CU ]
 then
-    part_arg="--partition=amilan "
-else
-    part_arg=""
+    partition_arg="--partition=amilan "
 fi
-array_job_id=$(sbatch --parsable --array=1-10 $part_arg array.sbatch)
-converge_job_id=$(sbatch --parsable --dependency=afterok:${array_job_id} $part_arg converge.sbatch $array_job_id)
+array_job_id=$(sbatch --parsable --array=1-10 $partition_arg array.sbatch)
+converge_job_id=$(sbatch --parsable --dependency=afterok:${array_job_id} $partition_arg converge.sbatch $array_job_id)
 job_monitor.sh $array_job_id,$converge_job_id

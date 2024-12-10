ee95f3948f1e9208d951ecfb80a8b41bb9d231d2
meekrob <davidcking.inbox@gmail.com>
2024-12-09 21:34:49 -0700

    increase number of array jobs

--- slurm_pipelines/array_w_convergence/launcher.sh
+++ slurm_pipelines/array_w_convergence/launcher.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-array_job_id=$(sbatch --parsable --array=1-5 array.sbatch)
+array_job_id=$(sbatch --parsable --array=1-10 array.sbatch)
 converge_job_id=$(sbatch --parsable --dependency=afterok:${array_job_id} converge.sbatch $array_job_id)
 job_monitor.sh $array_job_id,$converge_job_id

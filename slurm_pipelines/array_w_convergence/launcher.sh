566cb4d77b98ff09f51f65acd03d903593119f85
meekrob <davidcking.inbox@gmail.com>
2024-12-10 10:52:05 -0700

    updated array scripts

--- slurm_pipelines/array_w_convergence/launcher.sh
+++ slurm_pipelines/array_w_convergence/launcher.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/env bash
-
-array_job_id=$(sbatch --parsable --array=1-10 array.sbatch)
-converge_job_id=$(sbatch --parsable --dependency=afterok:${array_job_id} converge.sbatch $array_job_id)
+# launcher.sh - submit array job followed by converge script, dependent on array finishing afterok
+on_alpine=""
+if [ -n "$on_alpine" ]
+then
+    part_arg="--partition=amilan "
+else
+    part_arg=""
+fi
+array_job_id=$(sbatch --parsable --array=1-10 $part_arg array.sbatch)
+converge_job_id=$(sbatch --parsable --dependency=afterok:${array_job_id} $part_arg converge.sbatch $array_job_id)
 job_monitor.sh $array_job_id,$converge_job_id
